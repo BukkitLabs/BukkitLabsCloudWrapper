15eba3b1c77d7e0c98aebd5c8fb1c8bc299e6560
Asedem <asedem.games@gmail.com>
2023-05-20 15:25:42 +0200

    Initial commit 2

--- src/main/java/net/bukkitlabs/bukkitlabscloud/BukkitLabsCloudWrapper.java
+++ src/main/java/net/bukkitlabs/bukkitlabscloud/BukkitLabsCloudWrapper.java
@@ -1,6 +1,6 @@
-package net.bukkitlab;
+package net.bukkitlabs.bukkitlabscloud;
 
-public class Main {
+public class BukkitLabsCloudWrapper {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
